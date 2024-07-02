3c771e7f9bf1bb1b827e92ebcd3710469728c508
Mohamad Syakirin <130476382+Syakirin03@users.noreply.github.com>
2024-07-02 17:38:44 +0800

    Update protoype.php

--- php/protoype.php
+++ php/protoype.php
@@ -3,7 +3,6 @@ echo '<div class="main-content">
     <div class="left-frame hexagon-frame">
         <img src="images/frame-picture.jpg" alt="Frame Picture 1">
     </div>
-
     <div class="explore">
         <a href="http://localhost/test.html" target="_blank">
             <button>Explore</button>
